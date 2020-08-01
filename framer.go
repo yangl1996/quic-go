5676bb9bab6d9b59d1bb74d012d3ce843440bfb2
Lei Yang <lei@yangl1996.com>
2020-07-31 22:27:26 -0400

    strict prioritization

--- framer.go
+++ framer.go
@@ -7,6 +7,7 @@ import (
 	"github.com/lucas-clemente/quic-go/internal/protocol"
 	"github.com/lucas-clemente/quic-go/internal/utils"
 	"github.com/lucas-clemente/quic-go/internal/wire"
+	"container/heap"
 )
 
 type framer interface {
@@ -19,7 +20,41 @@ type framer interface {
 	AppendStreamFrames([]ackhandler.Frame, protocol.ByteCount) ([]ackhandler.Frame, protocol.ByteCount)
 }
 
-const MaxWeight int = 255
+type streamRef struct {
+	id protocol.StreamID
+	epoch int
+	index int
+}
+
+type streamQueue []streamRef
+
+func (pq streamQueue) Len() int { return len(pq) }
+
+func (pq streamQueue) Less(i, j int) bool {
+	return pq[i].epoch < pq[j].epoch
+}
+
+func (pq *streamQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(streamRef)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *streamQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n - 1]
+	item.index = -1
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (pq streamQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
 
 type framerI struct {
 	mutex sync.Mutex
@@ -28,7 +63,7 @@ type framerI struct {
 	version      protocol.VersionNumber
 
 	activeStreams map[protocol.StreamID]struct{}
-	streamQueue   []protocol.StreamID
+	streamQueue  streamQueue
 
 	controlFrameMutex sync.Mutex
 	controlFrames     []wire.Frame
@@ -87,8 +122,14 @@ func (f *framerI) AppendControlFrames(frames []ackhandler.Frame, maxLen protocol
 
 func (f *framerI) AddActiveStream(id protocol.StreamID) {
 	f.mutex.Lock()
+	str, err := f.streamGetter.GetOrOpenSendStream(id)
+	// The stream can be nil if it completed after it said it had data.
+	if str == nil || err != nil {
+		return // TODO
+		panic("hey")	// the stream is gone
+	}
 	if _, ok := f.activeStreams[id]; !ok {
-		f.streamQueue = append(f.streamQueue, id)
+		heap.Push(&f.streamQueue, streamRef{id, str.weight(), 0})
 		f.activeStreams[id] = struct{}{}
 	}
 	f.mutex.Unlock()
@@ -104,8 +145,8 @@ func (f *framerI) AppendStreamFrames(frames []ackhandler.Frame, maxLen protocol.
 		if protocol.MinStreamFrameSize+length > maxLen {
 			break
 		}
-		id := f.streamQueue[0]
-		f.streamQueue = f.streamQueue[1:]
+		ref := heap.Pop(&f.streamQueue).(streamRef)
+		id := ref.id
 		// This should never return an error. Better check it anyway.
 		// The stream will only be in the streamQueue, if it enqueued itself there.
 		str, err := f.streamGetter.GetOrOpenSendStream(id)
@@ -121,7 +162,7 @@ func (f *framerI) AppendStreamFrames(frames []ackhandler.Frame, maxLen protocol.
 		remainingLen += utils.VarIntLen(uint64(remainingLen))
 		frame, hasMoreData := str.popStreamFrame(remainingLen)
 		if hasMoreData { // put the stream back in the queue (at the end)
-			f.streamQueue = append(f.streamQueue, id)
+			heap.Push(&f.streamQueue, ref)
 		} else { // no more data to send. Stream is not active any more
 			delete(f.activeStreams, id)
 		}
